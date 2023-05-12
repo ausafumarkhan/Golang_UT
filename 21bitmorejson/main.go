b1b20a0c5044d7225332a8140b2d7a2436ec8dad
ausafumarkhan <ausafumarkhan@gmail.com>
2023-05-13 05:59:15 +0900

    Goalng Exercise

--- 21bitmorejson/main.go
+++ 21bitmorejson/main.go
@@ -15,7 +15,8 @@ type course struct {
 
 func main() {
 	fmt.Println("Welcome to JSON video")
-	EncodeJson()
+	//EncodeJson()
+	DecodeJson()
 }
 
 func EncodeJson() {
@@ -31,4 +32,38 @@ func EncodeJson() {
 	}
 
 	fmt.Printf("%s", finalJson)
+}
+
+func DecodeJson() {
+	jsonDataFromWeb := []byte(`
+	{
+		"coursename": "React Bootcamp",
+		"Price": 299,
+		"website": "Youtube",
+		"tags": ["beginner","web-dev"]
+	}
+	`)
+	var lcocourse course
+
+	// We can validate the json data (format) using inbuilt method
+
+	checkValid := json.Valid(jsonDataFromWeb)
+	if checkValid {
+		fmt.Println("Json was Valid")
+		json.Unmarshal(jsonDataFromWeb, &lcocourse)
+		fmt.Printf("%#v\n", lcocourse)
+	} else {
+		fmt.Println("Json was not valid")
+	}
+	// Some cases we just want to add data into key value pair
+	// key can be a string but there is no guarantee of value' datatype so we use "interface"
+
+	var myOnlineData map[string]interface{}
+	json.Unmarshal(jsonDataFromWeb, &myOnlineData)
+	fmt.Printf("%#v\n", myOnlineData)
+
+	for k, v := range myOnlineData {
+		fmt.Printf("Key is %v and value is %v. Tyep of value is %T\n", k, v, v)
+	}
+
 }
